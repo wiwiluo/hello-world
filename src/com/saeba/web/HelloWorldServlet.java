93ffc43a642ec573faed076a05d57c43726598a7
wiwiluo <saebalee@gmail.com>
2017-04-09 20:29:37 +0800

    增加User.java类

--- src/com/saeba/web/HelloWorldServlet.java
+++ src/com/saeba/web/HelloWorldServlet.java
@@ -1,9 +1,21 @@
 package com.saeba.web;
 
+import com.saeba.domain.User;
+
 /**
  * Created by wiwil on 2017/4/9.
  */
 public class HelloWorldServlet
 {
+
+    public static void main(String[] args)
+    {
+        User user = new User();
+        user.setUserID(1);
+        user.setUsername("77sama");
+        user.setAge(30);
+        user.setSex("男");
+        System.out.println(user);
+    }
 
 }
